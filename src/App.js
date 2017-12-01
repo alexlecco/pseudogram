d7d58312cd7a95984fa4e42389002c1a90280d7a
alexlecco <alexlecco24@gmail.com>
2017-12-01 18:48:43 -0300

    Google Auth was added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,53 @@ import firebase from 'firebase';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: null,
+    };
+
+    // we need to indicate which reference to "this" we want to react uses
+    this.handleAuth = this.handleAuth.bind(this);
+    this.handleLogout = this.handleLogout.bind(this);
+  }
+
+  componentWillMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ user }); // ({user: user})
+    });
+  }
+
   handleAuth() {
     const provider = new firebase.auth.GoogleAuthProvider();
 
     firebase.auth().signInWithPopup(provider)
       .then( result => console.log(`${result.user.email} ha iniciado sesión`) )
       .catch( error => console.log(`Error ${error.code}: ${error.message}`) );
+  }
+
+  handleLogout() {
+    firebase.auth().signOut()
+      .then( result => console.log(`${result.user.email} ha salido`) )
+      .catch( error => console.log(`Error ${error.code}: ${error.message}`) );
+  }
+
+  renderLoginButton() {
+    if(this.state.user) {
+      // if user is logged in
+      return(
+        <div>
+          <img width="100" src={this.state.user.photoURL} alt={this.state.user.displayName} />
+          <p> Hola {this.state.user.displayName}! </p>
+          <button onClick={this.handleLogout}> Salir </button>
+        </div>
+      );
+    } else {
+      // if user isn't logged in
+      return(
+        <button onClick={this.handleAuth}> Login con Google </button>
+      );
+    }
   }
 
   render() {
@@ -18,7 +59,7 @@ class App extends Component {
           <h1 className="App-title">Pseudogram</h1>
         </header>
         <p className="App-intro">
-          <button onClick={this.handleAuth}>Login con Google</button>
+          { this.renderLoginButton() }
         </p>
       </div>
     );
